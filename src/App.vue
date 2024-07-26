353264cc55327afaad6543addb6533fd5a8b9039
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-07-25 23:48:48 -0300

    REFACTOR: code restructuring and style improvements

--- src/App.vue
+++ src/App.vue
@@ -25,18 +25,50 @@ const botao = ref(false)
 </script>
 
 <template>
-   <div v-if="!botao">
-    <form>
+  <div >
+   <div class="containerApp" v-if="!botao">
     <FormEdit  :dados-edit="perfil"/>
    <button v-if="perfil.senha ==perfil.confSenha && perfil.senha.length >= 8" @click="botao = !botao" type="submit">{{ nomeBotao }}</button>
-    </form>
     </div>
-  <div v-if="botao">
+  <div class="container2" v-if="botao">
     <Infos :dados="perfil" />
     <button  @click="botao = !botao">{{ nomeBotao }}</button>
   </div>
+</div>
 </template>
 
 <style scoped>
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+ .containerApp{
+  background-image: linear-gradient(to right, #273cc1, #ffffff, rgb(14, 14, 159));
+  height: 100%;
+  width: 100%;
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2%;
+}
+.container2{
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  background-image: linear-gradient(to right, #273cc1, #ffffff, rgb(14, 14, 159));
+  height: 100vh;
+  width: 100vw;
+}
+ button{
+      margin-top: 20px;
+      padding: 10px;
+      color: white;
+      background-color: #0e52ff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+ }
 </style>
--- src/components/FormInfo.vue
+++ src/components/FormInfo.vue
@@ -3,7 +3,7 @@ defineProps(['dados']);
 
 </script>
 <template>
-    <div>
+    <div class="container">
         <div id="infosEnviadas">
                     <h1>SEUS DADOS</h1>
                     <p>Nome: {{ dados.nome }}</p>
@@ -20,4 +20,27 @@ defineProps(['dados']);
     </div>
 </template>
 <style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 200px;
+        margin: auto;
+    }
+    #infosEnviadas {
+        background: rgba( 255, 255, 255, 0.5 );
+        padding: 20px;
+        border-radius: 10px;
+        margin-top: 20px;
+    }
+    .Bio {
+        margin-top: 20px;
+    }
+    p {
+        margin: 10px 0;
+    }
+    h1 {
+        margin-bottom: 20px;
+    }
+
 </style>
